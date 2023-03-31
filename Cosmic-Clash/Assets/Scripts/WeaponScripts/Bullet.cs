6819051aa5cba354d5459930774113237f2ac6d7
Yuxuan-Lin <yl5445@nyu.edu>
2023-03-31 02:25:36 -0400

    Shotgun 1.0

--- Cosmic-Clash/Assets/Scripts/WeaponScripts/Bullet.cs
+++ Cosmic-Clash/Assets/Scripts/WeaponScripts/Bullet.cs
@@ -12,21 +12,20 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        rb = GetComponent<Rigidbody2D>();
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - transform.position;
-        Vector3 rotation = transform.position - mousePos;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        //mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        //rb = GetComponent<Rigidbody2D>();
+        //mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        //Vector3 direction = mousePos - transform.position;
+        //Vector3 rotation = transform.position - mousePos;
+        //rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (!other.gameObject.CompareTag("Bullet"))
         {
-            // Destroy this game object
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
     }
     void Update() {
 
