5728583e0c53d338094ad6cc405d3696c8aea8a0
Yuxuan-Lin <yl5445@nyu.edu>
2023-04-06 00:09:49 -0400

    Drone 1.0 Complete

--- Cosmic-Clash/Assets/Scripts/DroneController.cs
+++ Cosmic-Clash/Assets/Scripts/DroneController.cs
@@ -9,7 +9,9 @@ public class DroneController : MonoBehaviour
     public float time = 0.05f;
     public float speed = 0.15f;
     public float detectDistance = 20f;
+    public float destroyDistance = 40f;
     private bool isFollowing = false;
+    private bool delivered = false;
 
 
     // Start is called before the first frame update
@@ -24,14 +26,23 @@ public class DroneController : MonoBehaviour
         Vector2 dronePos = transform.position;
         Vector2 playerPos = player.position;
         Vector2 targetPos = playerPos + new Vector2(0, 5f);
-        if (Vector2.Distance(dronePos, playerPos) < detectDistance) {
+        if (!delivered && Vector2.Distance(dronePos, playerPos) < detectDistance) {
             isFollowing = true;
         }
         if (isFollowing) {
             transform.position = Vector2.MoveTowards(dronePos, Vector2.Lerp(dronePos, targetPos, time), speed);
         }
+        if (!delivered) {
+            weapon.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        }
         if (Vector2.Distance(dronePos, targetPos) < 0.2) {
-            //Destroy(gameObject);
+            delivered = true;
+            isFollowing = false;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
+            weapon.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+        if (Vector2.Distance(dronePos, playerPos) > destroyDistance) {
+            Destroy(gameObject);
         }
     }
 }
