b54b39f78ca3a374ee5fb7ddf557c94d752f218d
Sergey <thankfulcon@gmail.com>
2023-04-14 04:06:49 -0400

    final touch

--- Cosmic-Clash/Assets/Scripts/patrol.cs
+++ Cosmic-Clash/Assets/Scripts/patrol.cs
@@ -9,34 +9,25 @@ public class patrol : MonoBehaviour
     private int _currentWaypointIndex = 0;
     private float _speed = 2f;
     private Vector3 scaleChange = new Vector3(-0.1f, 0.0f, 0.0f);
+
     private void Update()
     {
         Transform wp = waypoints[_currentWaypointIndex];
         if (Vector3.Distance(transform.position, wp.position) < 0.01f)
         {
             _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
-             StartCoroutine(rotate());
+            StartCoroutine(Rotate());
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, wp.position, _speed * Time.deltaTime);
         }
     }
-     private IEnumerator rotate()
+
+    private IEnumerator Rotate()
     {
-        float elapsedTime = 0f;
-        Quaternion initialRotation = transform.rotation;
-        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 180, 0);
-
-        while (transform.localScale.x <= shipDimensions && transform.localScale.x >= -shipDimensions)
-        {
-            transform.localScale += scaleChange;
-            elapsedTime += Time.deltaTime;
-            float t = elapsedTime / 3f;
-            yield return null;
-        }
-        transform.localScale += -scaleChange;
-        scaleChange.x = -scaleChange.x;
+        transform.rotation *= Quaternion.Euler(0, 180, 0);
+        yield return null;
     }
-
 }
+
